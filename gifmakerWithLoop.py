b7e24909b6749e0e3cf190ef08719f9269f499a3
Gökhan Karabulut <gknkrblt@hotmail.com>
2023-09-14 19:02:54 +0300

    gifmaker

---
+++ gifmakerWithLoop.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+image_path_list = ["stickman1.jpg", "stickman2.jpg", "stickman3.jpg", "stickman4.jpg"]
+image_list = [Image.open(file) for file in image_path_list]
+image_list[0].save(
+            'animation.gif',
+            save_all=True,
+            append_images=image_list[1:],
+            duration=240,
+            loop=0)
